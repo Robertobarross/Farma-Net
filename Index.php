dde8db7cd7db72c4ba1883be094e2bdea876afd8
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-21 11:47:51 -0300

    Criando o Index.php da página principal farma+Net

---
+++ Index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farma+Net</title>
+</head>
+<body>
+
+    <!-- Inicio da div global -->
+    <div id="Global">
+    <!-- Configuração da div global com css -->
+    <style type="text/css">
+    #Global{height: auto;}/* Autura automático */
+    body {background-color: red;} /* Cor de fundo vermelha */
+    </style>
+    
+
+    <div id="Topo">
+        Topo
+    </div>
+
+    <div id="Conteudo">
+        Conteúdo
+    </div>
+
+    <div id="Rodape">
+        Rodapé
+    </div>
+    
+
+    </div>
+    <!-- Inicio da div global -->
+
+
+    
+</body>
+</html>
