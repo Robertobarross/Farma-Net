e6da536c82492e11d873c3846821ad7060099cbf
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-21 22:44:23 -0300

    Trabalhando nas divs do Index.php

--- Index.php
+++ Index.php
@@ -10,28 +10,51 @@
 
     <!-- Inicio da div global -->
     <div id="Global">
+
     <!-- Configuração da div global com css -->
     <style type="text/css">
     #Global{height: auto;}/* Autura automático */
     body {background-color: red;} /* Cor de fundo vermelha */
+    .titulo{font-size: 60px; font-family: Arial; color: white;} /* Estilo dos titulos */
+    .subtitulo{font-size: 20px; font-family: Arial; font-style: italic; color: white;}/* Estilo do subtitulo */
+    #Conteudo{width: 70%; min-height: 500px; border: 1px solid; color: white; float: left;}     
+    #Imagem{width: 28%; min-height: 500px; border: 1px solid; color: white; float: right;}
+
     </style>
+    <!-- Fim da configuração da div global --> 
     
+<!-- inicio da div topo --> 
+<div id="Topo" align="center">
+    <!-- titulo e subtitulo -->
+    <a class="titulo">Farma+Net</a> <a class="subtitulo">Sua farmacia on-line</a>
+    <p><hr width="100%" size="01" color="white"></p><!-- Linha que divide o topo do conteúdo -->
+</div>
+<!-- Fim da div topo --> 
 
-    <div id="Topo">
-        Topo
-    </div>
 
-    <div id="Conteudo">
-        Conteúdo
-    </div>
+<!-- Inicio da div conteúdo -->
+<div id="Conteudo">
+    teste
+        
+</div>
+<!-- Inicio da div conteúdo -->
 
-    <div id="Rodape">
-        Rodapé
-    </div>
-    
+<!-- Inicio da div imagem --> 
+<div id="Imagem">
 
-    </div>
-    <!-- Inicio da div global -->
+
+</div>
+<!-- Fim da div imagem --> 
+
+<!-- Inicio da div rodapé -->
+<div id="Rodape">
+    <p><hr width="100%" size="01" color="Red"></p><!-- Linha que divide o topo do conteúdo -->   
+</div>
+<!-- Fim da div rodapé -->
+
+
+</div>
+    <!-- Fim da div global -->
 
 
     
