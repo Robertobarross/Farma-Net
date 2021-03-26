98a3dc36cde46b2bf6c8daef6e872aaa3eedac0f
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-25 23:28:35 -0300

    Formulário de pesquisa

---
+++ FormBuscar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farma+Net</title>
+</head>
+<body>
+
+<!-- Inicio da div Cadstro -->
+<div id="Cadastro" align="Center">
+
+    <!-- Formulário com o campo de pesquisa -->
+    <form name="searchform" method="POST" action="Buscar.php">
+    <input type="search" name="buscar" class="buscar" placeholder="Pesquisar">    
+    </form>
+
+    <style type="text/css"> /* Configuração do campo de pesquisa */
+    .buscar{width: 35%; font-family: Arial; margin-top: 8px; border-radius: 8px; border: gray; min-height: 25px; background-color: pink;}
+    </style>
+
+</div>
+    
+</body>
+</html>
