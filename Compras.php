51926446bddcc80c10762792ac5172be551a3d93
Roberto Barrosss <robertobarros27esp@gmail.com>
2021-03-30 22:15:38 -0300

    Iniciando página de compras

---
+++ Compras.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Farma+Net</title>
+</head>
+<body>
+
+<div id="Global"><!-- Inicio da div global -->
+
+    <style type="text/css">
+    body{background-image: url('imagens/web.png');}
+    </style>
+
+<div id="Compras"><!-- Inicio da div Compras -->
+
+    <h1 class="texto">Ops! Página em manutenção!</h1>
+
+    <style type="text/css">
+    .texto{font-size: 150px; color: #99ff00;} /* Estilo do texto */
+    </style>
+
+</div><!-- Fim da div compras -->
+
+</div><!-- Fim da div global -->
+    
+</body>
+</html>
